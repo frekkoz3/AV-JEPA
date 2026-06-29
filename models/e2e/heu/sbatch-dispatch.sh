55b291756477b4674f2458b3f0d72b953a4e7a66
enricosavorgnan <enricosavorgnan@gmail.com>
2026-06-29 16:00:05 +0200

    add final (hopefully) models

--- models/e2e/heu/sbatch-dispatch.sh
+++ models/e2e/heu/sbatch-dispatch.sh
@@ -3,11 +3,11 @@
 epochs=$1
 
 # 1. Submit the first job without dependencies
-job_id=$(sbatch ./models/e2e/final/sbatch.sh | cut -d ' ' -f 4)
+job_id=$(sbatch ./models/e2e/heu/sbatch.sh | cut -d ' ' -f 4)
 echo "Submitted initial job (Run 1): $job_id"
 
 # 2. Loop for the remaining epochs
 for (( i=2; i<=epochs; i++ )); do
-    job_id=$(sbatch --dependency=afterok:$job_id ./models/e2e/final/sbatch.sh | cut -d ' ' -f 4)
+    job_id=$(sbatch --dependency=afterok:$job_id ./models/e2e/heu/sbatch.sh | cut -d ' ' -f 4)
     echo "Submitted dependent job (Run $i): $job_id"
 done
